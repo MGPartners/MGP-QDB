da39796ae200f98df2ebb9e849a91470c290a5cf
Eli Lee Kitagawa <cosmosart@msn.com>
2025-01-18 14:19:06 +0900

    Modified question types

--- apps/utils.py
+++ apps/utils.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from datetime import datetime
+import datetime
 from random import getrandbits
-from .configs.mappings import Mappings
+from .configs.mappings import Mappings, SystemMapping, QuestionMapping
 
 def generate_id(subject: str, level: str, question_type: str, user_type: str, uid: str) -> str:
-        date = datetime.now().strftime("%Y%m%d")
+        date = datetime.datetime.now().strftime("%Y%m%d")
         return f"{subject}-{level}-{question_type}-{user_type}-{uid.split('@')[0]}-{date}-{getrandbits(16)}"
 
 def init() -> None:
@@ -19,7 +19,7 @@ def init() -> None:
     )
 
 def render_sidebar(uid: str) -> tuple:
-    st.sidebar.markdown(f"User ID: {uid}")
+    st.sidebar.text(f"User ID: {uid}")
     subject = st.sidebar.selectbox("Subject", ["英検"])
     level = st.sidebar.selectbox("Level", ["３級", "準２級", "２級", "準１級", "１級"])
     question_type = st.sidebar.selectbox("Question type", ["英作文", "英文要約", "Ｅメール"])
@@ -36,48 +36,81 @@ def mapping_data(uid: str) -> tuple:
         mappings["user_type_map"][user_type]
     )
 
+
+def create_question_form(question_type: str):
+    question = st.text_area("Question", key=f"question_text_area")
+    if question_type == "writing":
+        question_point = st.text_area("Question Point", key="question_point_text_area").split("\n")
+        etc = None
+    elif question_type == "summarizing":
+        question_point = None
+        etc = None
+    elif question_type == "emailing":
+        question_point = None
+        etc = st.text_area("Etc", key="question_etc_text_area")
+    else:
+        raise ValueError("Invalid question type")
+    col1, col2 = st.columns(2)
+    min_words = col1.number_input("Min Words", min_value=20, max_value=200, value=40, key="input_min_words")
+    max_words = col2.number_input("Max Words", min_value=20, max_value=200, value=60, key="input_max_words")
+    return {
+        "question": question,
+        "question_point": question_point,
+        "etc": etc,
+        "min_words": min_words,
+        "max_words": max_words
+    }
+
+def rander_question_preview(question_type, question_dict) -> None:
+    if question_type == "writing":
+        topic = QuestionMapping.writing
+    elif question_type == "summarizing":
+        topic = QuestionMapping.summarizing
+    elif question_type == "emailing":
+        topic = QuestionMapping.emailing
+
+
+    st.markdown(topic)
+    st.markdown("""---""")
+    st.markdown("""#### Preview""")
+    st.markdown(f"""- {topic}""")
+    st.markdown(f"""- {QuestionMapping.number_of_words.format(min_words = question_dict['min_words'],
+                                                         max_words = question_dict['max_words'])}""")
+    st.markdown(f"""- {QuestionMapping.Warning_summarizing}""")
+    st.markdown("""---""")
+    st.markdown(f"""
+                {question_dict["question"]}
+    """)
+    st.markdown(f"""- {'\n- '.join(question_dict["question_point"])}""") if question_dict["question_point"] else None
+    st.markdown(f"""- {question_dict["etc"]}""") if question_dict["etc"] else None
+    st.markdown("""---""")
+
 def rander_question_form(uid: str) -> None:
     subject, level, question_type, user_type = mapping_data(uid)
     docs_id = generate_id(subject, level, question_type, user_type, uid)
-    st.markdown(docs_id)
-                
-    question = st.text_area("Question")
-    question_point = st.text_area("Question Point")
-    instruction = st.text_area("Instruction")
-    min_words = st.text_input("Min Words", value="40")
-    max_words = st.text_input("Max Words", value="50")
+    question_dict = create_question_form(question_type)
+    rander_question_preview(question_type, question_dict)
 
-    st.markdown("""---""")
-    st.markdown("""### Preview""")
-    st.markdown(f"""#### Question ID: 
-                {docs_id}""")
-    st.markdown(f"""
-                {question}
-    """)
-    st.markdown(f"""- {question_point}""")
-    st.markdown(f"""- {instruction}""")
-    st.markdown(f"""- {min_words}""")
-    st.markdown(f"""- {max_words}""")
-    st.markdown("""---""")
-    st.markdown("""### Submit""")
     if st.button("Submit"):
         data = {
             "id": docs_id,
-            "question": question,
-            "question_point": question_point,
-            "instruction": instruction,
-            "min_words": min_words,
-            "max_words": max_words,
+            "question": question_dict["question"],
+            "question_point": question_dict["question_point"],
+            "etc": question_dict["etc"],
+            "min_words": int(question_dict["min_words"]),
+            "max_words": int(question_dict["max_words"]),
             "subject": subject,
             "level": level,
             "question_type": question_type,
-            "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "created_at": datetime.datetime.now(tz=datetime.timezone.utc).timestamp(),
             "created_by": uid,
             "user_type": user_type
         }
         st.markdown(data)
-        # response = requests.post(f"{api_endpoint}/add_question", json=data)
+        # response = requests.post(f"{SystemMapping.api_endpoints}/add_question", json=data)
         # if response.status_code == 200:
         #     st.markdown("Question added successfully")
         # else:
         #     st.markdown("Error adding question") 
+        st.markdown(f"""#### Submitted successfully. Question ID is below: 
+                    {docs_id}""")
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import requests
 import streamlit as st
-from random import getrandbits
 from apps import authenticate, utils
-from apps.configs import mappings
 
 api_endpoint = "http://192.168.77.84:8000"
 
@@ -14,48 +11,3 @@ if logged_in:
     utils.init()
 
     utils.rander_question_form(uid)
-
-    # subject, level, question_type, user_type = utils.mapping_data(uid)
-    # docs_id = utils.generate_id(subject, level, question_type, user_type, uid)
-    # st.markdown(docs_id)
-                
-    # question = st.text_area("Question")
-    # question_point = st.text_area("Question Point")
-    # instruction = st.text_area("Instruction")
-    # min_words = st.text_input("Min Words", value="40")
-    # max_words = st.text_input("Max Words", value="50")
-
-    # st.markdown("""---""")
-    # st.markdown("""### Preview""")
-    # st.markdown(f"""#### Question ID: 
-    #             {docs_id}""")
-    # st.markdown(f"""
-    #             {question}
-    # """)
-    # st.markdown(f"""- {question_point}""")
-    # st.markdown(f"""- {instruction}""")
-    # st.markdown(f"""- {min_words}""")
-    # st.markdown(f"""- {max_words}""")
-    # st.markdown("""---""")
-    # st.markdown("""### Submit""")
-    # if st.button("Submit"):
-    #     data = {
-    #         "id": docs_id,
-    #         "question": question,
-    #         "question_point": question_point,
-    #         "instruction": instruction,
-    #         "min_words": min_words,
-    #         "max_words": max_words,
-    #         "subject": subject,
-    #         "level": level,
-    #         "question_type": question_type,
-    #         "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    #         "created_by": uid,
-    #         "user_type": user_type
-    #     }
-    #     st.markdown(data)
-    #     response = requests.post(f"{api_endpoint}/add_question", json=data)
-    #     if response.status_code == 200:
-    #         st.markdown("Question added successfully")
-    #     else:
-    #         st.markdown("Error adding question")
