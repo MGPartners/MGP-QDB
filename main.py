d3f12374df71382d16b19856a1d36c1ffd7a95ae
Eli Lee Kitagawa <cosmosart@msn.com>
2025-01-19 20:21:13 +0900

    add debug logic

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import streamlit as st
 from apps import authenticate, utils
+import os
+
+# Debug logic to check paths
+file_path = "/app/main.py"
+if os.path.exists(file_path):
+    st.write(f"File exists: {file_path}")
+else:
+    st.write(f"File does not exist: {file_path}")
+
+# List all files in the directory
+directory_path = "/usr/src/app/"
+if os.path.isdir(directory_path):
+    st.write(f"Directory exists: {directory_path}")
+    files = os.listdir(directory_path)
+    st.write("Files in directory:")
+    for file in files:
+        st.write(file)
+else:
+    st.write(f"Directory does not exist: {directory_path}")
+
+directory_path = "/"
+if os.path.isdir(directory_path):
+    st.write(f"Directory exists: {directory_path}")
+    files = os.listdir(directory_path)
+    st.write("Files in directory:")
+    for file in files:
+        st.write(file)
+else:
+    st.write(f"Directory does not exist: {directory_path}")
+
 
 api_endpoint = "https://mgpta.asia-northeast2.run.app/"
 
