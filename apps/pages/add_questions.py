21d2329673d5b28b3ac1bea2ef523de275de491c
Eli Lee Kitagawa <cosmosart@msn.com>
2025-07-06 21:51:26 +0900

    fix email bug from add question

--- apps/pages/add_questions.py
+++ apps/pages/add_questions.py
@@ -5,7 +5,18 @@ st.markdown("# Add Questions")
 
 if st.session_state.get("logged_in"):
     utils.init()
-    subject, exam_grade, question_type, user_type, official = utils.mapping_data(st.session_state.uid)
+    # Use original Japanese labels for selectbox
+    subject = st.sidebar.selectbox("Subject", ["英検"])
+    exam_grade = st.sidebar.selectbox("Grade", ["３級", "準２級", "２級", "準１級", "１級"])
+    question_type_label = st.sidebar.selectbox("Question type", ["英作文", "英文要約", "Ｅメール"])
+    user_type = st.sidebar.selectbox("User type", ["Teacher", "Student"])
+    official = st.sidebar.checkbox("Official", value=False)
+    # Map 'Ｅメール' to 'email' for backend
+    data_map = utils.mappings.Mappings().get_mappings()
+    subject = data_map["subject_map"][subject]
+    exam_grade = data_map["grade_map"][exam_grade]
+    question_type = 'email' if question_type_label == 'Ｅメール' else data_map["question_type_map"][question_type_label]
+    user_type = data_map["user_type_map"][user_type]
     docs_id = utils.generate_id(subject, exam_grade, question_type, user_type, st.session_state.uid)
 
     # Use unified form logic from utils
